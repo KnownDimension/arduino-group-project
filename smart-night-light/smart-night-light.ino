b5a9e2a4489909d8d268a742b3c909ce88eabf18
KnownDimension <tim.heinzle@outlook.com>
2026-03-12 17:51:29 +0000

    something something complete task2 tested even

--- smart-night-light/smart-night-light.ino
+++ smart-night-light/smart-night-light.ino
@@ -1,12 +1,18 @@
-#include <string>
+
 
 // Light Switch
 int sensorpin = A6;
-int ledPin = 4;
+int ledPin = 3;
+int buttonPin = 6;
 int sensorValue = 0;
 int outputValue = 0;
 int rotaryPin = A0;
-
+int rotaryValue = 0;
+bool trueVal = false;
+int buttonState = 0;
+int brightness = 0;
+int ledbright = 0;
+bool buttonLock = false;
 
 
 #include <U8x8lib.h>
@@ -16,9 +22,11 @@ U8X8_SSD1306_128X64_NONAME_HW_I2C u8x8(/* reset=*/ U8X8_PIN_NONE);
 
 
 void setup() {
-  pinMode(ledPin,OUTPUT);
+  u8x8.clear();
+  pinMode(ledPin, OUTPUT);
   pinMode(sensorpin, INPUT);
   pinMode(rotaryPin, INPUT);
+  pinMode(buttonPin, INPUT);
   u8x8.begin();
   Serial.begin(9600);
 }
@@ -28,20 +36,26 @@ void loop() {
   u8x8.setCursor(0, 0);
   sensorValue = analogRead(sensorpin);
   rotaryValue = analogRead(rotaryPin);
-
+  Serial.println(rotaryValue);
   Serial.println(sensorValue);
 
-  if (sensorValue < rotaryValue) {
-    digitalWrite(ledPin, HIGH);
-    trueVal = true
-  }
-  else {
-    digitalWrite(ledPin, LOW);
-    trueVal = false
-  }
 
+
+
+
+  if (sensorValue < rotaryValue) {
+    analogWrite(ledPin, 255 - sensorValue);
+    trueVal = true;
+  } else {
+    analogWrite(ledPin, 0);
+    trueVal = false;
+  };
+
+
+  
   u8x8.print(rotaryValue);
   u8x8.setCursor(0, 25);
   u8x8.print(trueVal);
+  u8x8.setFlipMode(1);
   delay(200);
 }
